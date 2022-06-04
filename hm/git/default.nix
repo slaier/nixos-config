26c8ed7d9232ab3a9c449fea2b0f3458d3d803e9
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-04 07:58:08 +0000

    hm(git): add a git log alias

--- hm/git/default.nix
+++ hm/git/default.nix
@@ -23,6 +23,7 @@
 
     aliases = {
       st = "status -sb";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
   };
 }
