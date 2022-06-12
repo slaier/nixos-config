c8bb68c63b8d71011655ab70d7fe31a09f176b91
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-12 03:15:29 +0000

    overlays: pass nix flake check

--- overlays/fcitx5/default.nix
+++ overlays/fcitx5/default.nix
@@ -1,6 +1,6 @@
-self: super: {
-  fcitx5 = super.fcitx5.overrideAttrs (_: prev:
-    assert (prev.version == "5.0.15") && (!self.lib.hasAttrByPath [ "patches" ] prev);
+final: prev: {
+  fcitx5 = prev.fcitx5.overrideAttrs (_: prevAttrs:
+    assert (prevAttrs.version == "5.0.15") && (!prev.lib.hasAttrByPath [ "patches" ] prevAttrs);
     {
       patches = [
         ./fix_font_dpi_setup_for_xcbmenu.patch
