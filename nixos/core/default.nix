67e39b3d22fcf9bf4899efe6854437e43b32c36d
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-18 17:57:33 +0800

    nixos: adjust the prior of my substituter

--- nixos/core/default.nix
+++ nixos/core/default.nix
@@ -5,16 +5,16 @@ mkMerge [
     nix.settings = {
       substituters = [
         "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-        "https://slaier.cachix.org"
         "https://indexyz.cachix.org"
         "https://nrdxp.cachix.org"
         "https://nix-community.cachix.org"
+        "https://slaier.cachix.org"
       ];
       trusted-public-keys = [
-        "slaier.cachix.org-1:NyXPOqlxuGWgyn0ApNHMopkbix3QjMUAcR+JOjjxLtU="
         "indexyz.cachix.org-1:biBEnuZ4vTSsVMr8anZls+Lukq8w4zTHAK8/p+fdaJQ="
         "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "slaier.cachix.org-1:NyXPOqlxuGWgyn0ApNHMopkbix3QjMUAcR+JOjjxLtU="
       ];
       auto-optimise-store = true;
     };
