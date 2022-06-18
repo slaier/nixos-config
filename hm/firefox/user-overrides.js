4250c29dcc0f8ba800bc7d823d4b4a558198e6ca
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-19 00:38:37 +0800

    hm: fix content blocking category is not compatible with AdNauseam

--- hm/firefox/user-overrides.js
+++ hm/firefox/user-overrides.js
@@ -4,4 +4,5 @@ user_pref("svg.context-properties.content.enabled", true);
 user_pref("network.trr.mode", 3); // enable TRR (without System fallback)
 user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 user_pref("network.security.esni.enabled", true);
+user_pref("browser.contentblocking.category", "standard");
 // end
