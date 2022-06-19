56e40d2b9e36f3e0db9340743e9679d65efdb4ab
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-19 08:20:29 +0800

    hm(firefox): keep cookie and history when shutdown

--- hm/firefox/user-overrides.js
+++ hm/firefox/user-overrides.js
@@ -5,4 +5,8 @@ user_pref("network.trr.mode", 3); // enable TRR (without System fallback)
 user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 user_pref("network.security.esni.enabled", true);
 user_pref("browser.contentblocking.category", "standard");
+/*** [SECTION 2800]: SHUTDOWN & SANITIZING ***/
+// 3: The cookie lasts for the number of days specified by network.cookie.lifetime.days(Default: 90). 
+user_pref("network.cookie.lifetimePolicy", 3);
+user_pref("privacy.clearOnShutdown.history", false);
 // end
