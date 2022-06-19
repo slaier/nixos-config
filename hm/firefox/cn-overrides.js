38b24257d59f0eca47cf45e895cdee4fc0b4b603
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-19 12:33:23 +0800

    hm(firefox): fix web games not work

--- hm/firefox/cn-overrides.js
+++ hm/firefox/cn-overrides.js
@@ -2,4 +2,7 @@
 user_pref("network.http.referer.XOriginPolicy", 0);
 user_pref("network.trr.uri", "https://dns.alidns.com/dns-query");
 user_pref("network.trr.bootstrapAddress", "223.5.5.5");
+/*** [SECTION 4500]: RFP (RESIST FINGERPRINTING) ***/
+user_pref("privacy.resistFingerprinting", false);
+user_pref("webgl.disabled", false);
 // end
