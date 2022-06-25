105c481a3eedad27f2eb130c1eed8f10280b4cf8
Slaier <30682486+Slaier@users.noreply.github.com>
2022-06-25 14:10:56 +0800

    hm: fix firefox trr

--- hm/firefox/default.js
+++ hm/firefox/default.js
@@ -6,8 +6,10 @@ user_pref("network.trr.mode", 3); // enable TRR (without System fallback)
 user_pref("network.dns.skipTRR-when-parental-control-enabled", false);
 user_pref("network.security.esni.enabled", true);
 user_pref("network.trr.uri", "https://dns.google/dns-query");
-user_pref("network.trr.custom_uri", "https://dns.google/dns-query");
-user_pref("network.trr.bootstrapAddr", "8.8.8.8");
+user_pref("network.trr.bootstrapAddress", "8.8.8.8");
+user_pref("network.trr.blacklist-duration", 15);
+user_pref("network.trr.request_timeout_mode_trronly_ms", 1500);
+user_pref("network.trr.wait-for-A-and-AAAA", false);
 /*** [SECTION 2700]: ETP (ENHANCED TRACKING PROTECTION) ***/
 user_pref("browser.contentblocking.category", "standard");
 /*** [SECTION 2800]: SHUTDOWN & SANITIZING ***/
