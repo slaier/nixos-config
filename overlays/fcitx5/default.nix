d318c6ec0d430cdf6b63944f70fb2c5e69d242dd
Slaier <30682486+Slaier@users.noreply.github.com>
2022-07-10 17:15:18 +0800

    fcitx5: remove nix env patch

--- overlays/fcitx5/default.nix
+++ overlays/fcitx5/default.nix
@@ -3,9 +3,7 @@ final: prev: {
     assert (prevAttrs.version == "5.0.15") && (!prev.lib.hasAttrByPath [ "patches" ] prevAttrs);
     {
       patches = [
-        ./fcitx5-with-nix-env-variable.patch
         ./fix_font_dpi_setup_for_xcbmenu.patch
       ];
     });
 }
-
