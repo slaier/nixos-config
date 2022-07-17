75b63739d18c3fff9a96f5f69a8bbd95f543c064
Slaier <30682486+Slaier@users.noreply.github.com>
2022-07-17 11:06:57 +0800

    hm(fish): add git clone test function

--- hm/shell/default.nix
+++ hm/shell/default.nix
@@ -2,9 +2,15 @@
   programs.fzf.enable = true;
   programs.fish = {
     enable = true;
+    functions = {
+      git-clone-test = ''
+        set -l tempd (mktemp -d)
+        timeout 30s git clone --bare https://github.com/jquery/jquery.git $tempd
+        rm -rf $tempd
+      '';
+    };
     shellAliases = {
       nixexpr = ''curl -w "%{url_effective}\n" -I -L -s -S $URL -o /dev/null https://nixos.org/channels/nixos-22.05/nixexprs.tar.xz'';
     };
   };
 }
-
