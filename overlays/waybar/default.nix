90f969554cf9a8121580bbf06abcecf6ddf9536a
Slaier <30682486+Slaier@users.noreply.github.com>
2022-07-30 10:51:21 +0800

    overlays: remove waybar

--- overlays/waybar/default.nix
+++
@@ -1,9 +0,0 @@
-final: prev: {
-  waybar = prev.waybar.overrideAttrs (_: prevAttrs:
-    assert (prevAttrs.version == "0.9.12") && (!prev.lib.hasAttrByPath [ "patches" ] prevAttrs);
-    {
-      patches = [
-        ./fix_clock.patch
-      ];
-    });
-}
