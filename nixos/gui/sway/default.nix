74b4f07cd1d768f369ad24cc0731e5eda5586972
Slaier <30682486+Slaier@users.noreply.github.com>
2022-09-26 23:03:45 +0800

    nixos(sway): support switching to the last focused window

--- nixos/gui/sway/default.nix
+++ nixos/gui/sway/default.nix
@@ -3,29 +3,17 @@ let
   jq = "${pkgs.jq}/bin/jq";
   rofi = "${pkgs.rofi-wayland}/bin/rofi";
 
-  # https://gist.github.com/lbonn/89d064cde963cfbacabd77e0d3801398
   sway-window-switcher = pkgs.writeTextFile {
     name = "sway-window-switcher";
     destination = "/bin/sway-window-switcher";
     executable = true;
 
     text = ''
-      set -euo pipefail
-
-      tree=$(swaymsg -t get_tree)
-      readarray -t win_ids <<< "$(${jq} -r '.. | objects | select(has("app_id")) | .id' <<< "$tree")"
-      readarray -t win_names <<< "$(${jq} -r '.. | objects | select(has("app_id")) | .name' <<< "$tree")"
-      readarray -t win_types <<< "$(${jq} -r '.. | objects | select(has("app_id")) | .app_id // .window_properties.class' <<< "$tree")"
-
-      switch () {
-          local k
-          read -r k
-          swaymsg "[con_id=''${win_ids[$k]}] focus"
-      }
-
-      for k in $(seq 0 $((''${#win_ids[@]} - 1))); do
-          echo -e "<span weight=\"bold\">''${win_types[$k]}</span> - ''${win_names[$k]}"
-      done | ${rofi} -dmenu -markup-rows -i -p window -format i | switch
+      swaymsg -t get_tree |
+      ${jq} 'until(.type == "con";
+              .focus[:2][-1] as $id | .nodes[] | select(.id == $id)) |
+             select(.focused == false) | .id' |
+      xargs -r -I{} swaymsg "[con_id={}] focus"
     '';
   };
 
