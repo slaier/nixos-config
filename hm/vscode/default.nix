f6382bc829510dd3540c8ada25102a6c6e261a22
Slaier <30682486+Slaier@users.noreply.github.com>
2022-10-09 22:45:19 +0800

    hm(vscode): remove language-specific extensions

--- hm/vscode/default.nix
+++ hm/vscode/default.nix
@@ -6,19 +6,13 @@
       with pkgs.vscode-extensions; with pkgs.nur.repos.slaier; [
         eamodio.gitlens
         jnoortheen.nix-ide
-        matklad.rust-analyzer
-        ms-python.python
         ms-vscode-remote.remote-ssh
-        ms-vscode.cpptools
         redhat.vscode-yaml
+        shardulm94.trailing-spaces
         tamasfe.even-better-toml
         tyriar.sort-lines
 
-        vscode-extension-bmalehorn-vscode-fish
-        vscode-extension-ccls-project-ccls
         vscode-extension-ms-vscode-remote-remote-containers
-        vscode-extension-shardulm94-trailing-spaces
-        vscode-extension-timonwong-shellcheck
         vscode-extension-vscode-icons-team-vscode-icons
       ]
     );
@@ -32,14 +26,6 @@
       "editor.rulers" = [ 80 120 ];
       "extensions.autoCheckUpdates" = false;
       "extensions.autoUpdate" = false;
-      "files.exclude" = {
-        "**/.git" = true;
-        "**/.svn" = true;
-        "**/.hg" = true;
-        "**/CVS" = true;
-        "**/.DS_Store" = true;
-        "**/.ccls-cache" = true;
-      };
       "files.insertFinalNewline" = true;
       "files.trimFinalNewlines" = true;
       "files.trimTrailingWhitespace" = true;
@@ -55,18 +41,9 @@
       };
       "workbench.iconTheme" = "vscode-icons";
 
-      "C_Cpp.autocomplete" = "Disabled";
-      "C_Cpp.errorSquiggles" = "Disabled";
-      "C_Cpp.formatting" = "Disabled";
-      "C_Cpp.intelliSenseEngine" = "Disabled";
-      "ccls.highlight.function.face" = [ "enabled" ];
-      "ccls.highlight.type.face" = [ "enabled" ];
-      "ccls.highlight.variable.face" = [ "enabled" ];
-      "ccls.launch.command" = "${pkgs.ccls}/bin/ccls";
       "nix.enableLanguageServer" = true;
       "nix.serverPath" = "${pkgs.rnix-lsp}/bin/rnix-lsp";
       "remote.containers.dockerPath" = "podman";
-      "shellcheck.executablePath" = "${pkgs.shellcheck}/bin/shellcheck";
     };
   };
 }
