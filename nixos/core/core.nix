9c6046e942b2d8e7da5ef5d4b591777026ef33d7
slaier <30682486+slaier@users.noreply.github.com>
2022-11-22 22:17:29 +0800

    nixos: enable auto gc

--- nixos/core/core.nix
+++ nixos/core/core.nix
@@ -13,6 +13,8 @@
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
+    min-free = ${toString (100 * 1024 * 1024)}
+    max-free = ${toString (1024 * 1024 * 1024)}
   '';
 
   environment.systemPackages = with pkgs; [
