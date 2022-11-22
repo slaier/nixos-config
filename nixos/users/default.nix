10114143886a8e3da581a21daad75a58caa0f5a3
slaier <30682486+slaier@users.noreply.github.com>
2022-11-22 22:17:29 +0800

    nixos: squash users dir

--- nixos/users/default.nix
+++ nixos/users/default.nix
@@ -1,6 +1,15 @@
-{ ... }: {
-  imports = [
-    ./nixos
-  ];
-}
+{ pkgs, ... }: {
+  programs.fish.enable = true;
 
+  users.users.nixos = {
+    isNormalUser = true;
+    extraGroups = [
+      "adbusers"
+      "aria2"
+      "docker"
+      "vboxusers"
+      "wheel"
+    ];
+    shell = pkgs.fish;
+  };
+}
