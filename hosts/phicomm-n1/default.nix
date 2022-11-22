73bf2cc880f384820e968b62247753e35656f62b
slaier <30682486+slaier@users.noreply.github.com>
2022-11-22 22:17:29 +0800

    hosts(n1): add hardware configuration

--- hosts/phicomm-n1/default.nix
+++ hosts/phicomm-n1/default.nix
@@ -1,7 +1,20 @@
 { ... }: {
   imports = [
+    ./hardware-configuration.nix
     ../../nixos/core
     ../../nixos/users
   ];
+
+  # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
+  boot.loader.grub.enable = false;
+  # Enables the generation of /boot/extlinux/extlinux.conf
+  boot.loader.generic-extlinux-compatible.enable = true;
+
+  services.openssh.enable = true;
+
   networking.hostName = "n1";
+  networking.proxy = {
+    default = "http://127.0.0.1:7890";
+    noProxy = "127.0.0.1,localhost,192.168.0.0/16";
+  };
 }
