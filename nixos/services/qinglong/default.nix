41c428587ae6a279d563dc635a8052b4bb7bb4c0
slaier <30682486+slaier@users.noreply.github.com>
2022-11-27 15:37:59 +0800

    nixos(qinglong): create data directory

--- nixos/services/qinglong/default.nix
+++ nixos/services/qinglong/default.nix
@@ -1,9 +1,12 @@
-{ ... }: {
+{ lib, ... }: {
   virtualisation.oci-containers.containers.qinglong = {
     image = "docker.io/whyour/qinglong:latest";
     volumes = [
-      "/etc/qinglong:/ql/data"
+      "/var/lib/qinglong:/ql/data"
     ];
     ports = [ "5700:5700" ];
   };
+  system.activationScripts.qinglong = lib.stringAfter [ "var" ] ''
+    mkdir -p /var/lib/qinglong
+  '';
 }
