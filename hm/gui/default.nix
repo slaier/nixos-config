f27f91dfbcf10d6fc6d9e28eb2a5f0961660891f
Slaier <slaier@users.noreply.github.com>
2022-12-04 19:52:49 +0800

    hm(gtk): set gtk theme

--- hm/gui/default.nix
+++ hm/gui/default.nix
@@ -1,7 +1,19 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./firefox
     ./fcitx.nix
     ./vscode.nix
   ];
+
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      package = pkgs.capitaine-cursors;
+      name = "capitaine-cursors";
+    };
+    theme = {
+      package = pkgs.orchis-theme;
+      name = "Orchis-Light";
+    };
+  };
 }
