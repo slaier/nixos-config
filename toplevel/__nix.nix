3991b8bdfd91c434e4900f7a5acb6a31aebcae23
Slaier <slaier@users.noreply.github.com>
2023-04-09 14:58:41 +0800

    nix: add nix-community binary cache

--- toplevel/__nix.nix
+++ toplevel/__nix.nix
@@ -5,9 +5,11 @@
     substituters = [
       "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://slaier.cachix.org"
+      "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
       "slaier.cachix.org-1:NyXPOqlxuGWgyn0ApNHMopkbix3QjMUAcR+JOjjxLtU="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
     auto-optimise-store = true;
     flake-registry = "/etc/nix/registry.json";
