04a8680ed1317c1c4c9340912ec1fa746f672caa
Slaier <slaier@users.noreply.github.com>
2023-04-16 22:55:42 +0800

    bluetooth: enable battery level reporting in bluez

--- src/bluetooth/default.nix
+++ src/bluetooth/default.nix
@@ -2,9 +2,13 @@
 {
   hardware.bluetooth = {
     enable = true;
+    package = pkgs.bluez.override {
+      withExperimental = true;
+    };
     settings = {
       General = {
         Enable = "Source,Sink,Media,Socket";
+        Experimental = "true";
       };
     };
   };
