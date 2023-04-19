37a8913727c2e692b6fd68a2e0cf0d8a22cc974c
Slaier <slaier@users.noreply.github.com>
2023-04-19 21:57:00 +0800

    flake: add nix-ld

--- flake.nix
+++ flake.nix
@@ -22,6 +22,10 @@
       url = "github:nix-community/haumea";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    nix-ld.url = "github:Mic92/nix-ld";
+    nix-ld.inputs.nixpkgs.follows = "nixpkgs";
+    nix-ld.inputs.utils.follows = "flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, haumea, ... } @inputs:
--- toplevel/nixosConfigurations.nix
+++ toplevel/nixosConfigurations.nix
@@ -3,12 +3,13 @@ with lib;
 flip mapAttrs super.hosts (hostName: host:
 let
   inherit (host) system;
-  inherit (inputs) impermanence darkmatter-grub-theme home-manager nur nixpkgs;
+  inherit (inputs) impermanence darkmatter-grub-theme home-manager nur nixpkgs nix-ld;
   nurModules = import nur { nurpkgs = import nixpkgs { inherit system; }; };
 in
 nixosSystem {
   inherit system;
   modules = [
+    nix-ld.nixosModules.nix-ld
     impermanence.nixosModules.impermanence
     darkmatter-grub-theme.nixosModule
     home-manager.nixosModules.home-manager
