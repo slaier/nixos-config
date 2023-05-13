8fcf71e7f6d592f74b69d9c0c55c0a3a825ab4b9
Slaier <slaier@users.noreply.github.com>
2023-05-13 16:45:20 +0800

    docker: automatically update container images

--- modules/openfortivpn/default.nix
+++ modules/openfortivpn/default.nix
@@ -1,13 +1,7 @@
 { pkgs, ... }: {
   virtualisation.oci-containers.containers.openfortivpn = {
     image = "ghcr.io/slaier/openfortivpn:latest";
-    imageFile = pkgs.dockerTools.pullImage {
-      imageName = "ghcr.io/slaier/openfortivpn";
-      imageDigest = "sha256:12f9347274251cafa080a93d98e8645382b0fee36e2d55f97dd012e877e14dac";
-      sha256 = "sha256-m+XsPBinsOI864hwZWqW0uBvVkgjZIhlPpyMXYQqP4U=";
-      os = "linux";
-      arch = "arm64";
-    };
+    imageFile = pkgs.dockerTools.pullImage (import ./source.nix);
     volumes = [
       "/etc/openfortivpn/config:/etc/openfortivpn/config"
     ];
