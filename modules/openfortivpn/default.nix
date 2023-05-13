efedbd95112ca19eef59ea5500c48dacb451a732
Slaier <slaier@users.noreply.github.com>
2023-05-13 15:15:42 +0800

    openfortivpn: bypass pulling image from ghcr.io

--- modules/openfortivpn/default.nix
+++ modules/openfortivpn/default.nix
@@ -1,6 +1,13 @@
-{ ... }: {
+{ pkgs, ... }: {
   virtualisation.oci-containers.containers.openfortivpn = {
-    image = "ghcr.dockerproxy.com/slaier/openfortivpn:latest";
+    image = "ghcr.io/slaier/openfortivpn:latest";
+    imageFile = pkgs.dockerTools.pullImage {
+      imageName = "ghcr.io/slaier/openfortivpn";
+      imageDigest = "sha256:12f9347274251cafa080a93d98e8645382b0fee36e2d55f97dd012e877e14dac";
+      sha256 = "sha256-m+XsPBinsOI864hwZWqW0uBvVkgjZIhlPpyMXYQqP4U=";
+      os = "linux";
+      arch = "arm64";
+    };
     volumes = [
       "/etc/openfortivpn/config:/etc/openfortivpn/config"
     ];
