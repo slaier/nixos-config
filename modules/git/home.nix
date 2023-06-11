8bd1494c8245885c19a9ad6b93ab64210716c264
slaier <slaier@users.noreply.github.com>
2023-06-11 21:49:49 +0800

    git: simplify configuration

--- modules/git/home.nix
+++ modules/git/home.nix
@@ -1,61 +1,42 @@
-{ config, lib, pkgs, nixosConfig, ... }:
-
-with lib;
-let
-  cfg = config.slaier.git;
-in
+{ config, pkgs, nixosConfig, ... }:
 {
-  options.slaier.git = {
-    userName = mkOption {
-      type = types.str;
-      default = "Slaier";
-      description = "git user name";
-    };
-    userEmail = mkOption {
-      type = types.str;
-      default = "slaier@users.noreply.github.com";
-      description = "git user email";
-    };
+  sops.secrets.github_token = {
+    path = "${config.xdg.configHome}/git/credentials";
   };
 
-  config = {
-    sops.secrets.github_token = {
-      path = "${config.xdg.configHome}/git/credentials";
+  programs.git = {
+    enable = true;
+    userName = "slaier";
+    userEmail = "slaier@users.noreply.github.com";
+    diff-so-fancy = {
+      enable = true;
+    };
+    extraConfig = {
+      credential.helper = "store";
+      init.defaultBranch = "main";
+      merge.conflictstyle = "diff3";
+      merge.ff = "only";
+      pull.rebase = true;
+      gpg.format = "ssh";
+      gpg.ssh.allowedSignersFile = ''${pkgs.writeText "allowedSigners.txt"
+          ''${config.programs.git.userEmail} ${nixosConfig.programs.ssh.knownHosts."local.local".publicKey}''}'';
+      user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519";
+      commit.gpgsign = true;
     };
 
-    programs.git = {
-      inherit (cfg) userName userEmail;
+    ignores = [
+      ".cache"
+      ".direnv"
+    ];
 
-      enable = true;
-      diff-so-fancy = {
-        enable = true;
-      };
-      extraConfig = {
-        credential.helper = "store";
-        init.defaultBranch = "main";
-        merge.conflictstyle = "diff3";
-        merge.ff = "only";
-        pull.rebase = true;
-        gpg.format = "ssh";
-        gpg.ssh.allowedSignersFile = ''${pkgs.writeText "allowedSigners.txt"
-          ''${config.programs.git.userEmail} ${nixosConfig.programs.ssh.knownHosts."local.local".publicKey}''}'';
-        user.signingkey = "${config.home.homeDirectory}/.ssh/id_ed25519";
-        commit.gpgsign = true;
-      };
-
-      ignores = [
-        ".ccls-cache/"
-      ];
-
-      aliases = {
-        d = "diff";
-        dc = "diff --cached";
-        ds = "diff --staged";
-        r = "restore";
-        rs = "restore --staged";
-        st = "status -sb";
-        lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      };
+    aliases = {
+      d = "diff";
+      dc = "diff --cached";
+      ds = "diff --staged";
+      r = "restore";
+      rs = "restore --staged";
+      st = "status -sb";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
     };
   };
 }
