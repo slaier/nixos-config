db7188a666b7ba4842e1406ceafe4891127daa08
slaier <slaier@users.noreply.github.com>
2026-04-16 01:55:03 +0800

    perf(hosts/local): Enable BBR and tune network kernel parameters

--- hosts/local/default.nix
+++ hosts/local/default.nix
@@ -27,6 +27,16 @@
       noProxy = "127.0.0.1,localhost,.lan";
     };
   };
+  boot.kernel.sysctl = {
+    "net.core.default_qdisc" = "fq";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.ipv4.tcp_window_scaling" = 1;
+    "net.ipv4.tcp_rmem" = "4096 87380 134217728";
+    "net.ipv4.tcp_wmem" = "4096 16384 134217728";
+    "net.core.rmem_max" = 134217728;
+    "net.core.wmem_max" = 134217728;
+    "net.ipv4.route.flush" = 1;
+  };
 
   documentation.man.generateCaches = false;
 
