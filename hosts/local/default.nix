d9b606b7cfae6daf464e2bd203f37bc82bb54227
slaier <slaier@users.noreply.github.com>
2026-04-19 15:24:27 +0800

    feat(modules/shell/home.nix): add Ghostty, customize settings

--- hosts/local/default.nix
+++ hosts/local/default.nix
@@ -69,7 +69,6 @@
     filezilla
     freerdp
     # cli
-    alacritty
     bottom
     cloudflared
     curl
--- modules/shell/home.nix
+++ modules/shell/home.nix
@@ -6,4 +6,14 @@
       noproxy = "env -u http_proxy -u https_proxy -u ftp_proxy -u rsync_proxy -u all_proxy ";
     };
   };
+  programs.ghostty = {
+    enable = true;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    settings = {
+      theme = "Monokai Classic";
+      notify-on-command-finish = "unfocused";
+      notify-on-command-finish-action = "notify";
+    };
+  };
 }
