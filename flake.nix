33352dad95d6766dd95ed20eac9915783f8c14e3
slaier <slaier@users.noreply.github.com>
2026-05-11 23:01:39 +0800

    refactor: move installer config to dedicated module

--- flake.nix
+++ flake.nix
@@ -101,32 +101,15 @@
           })
         ];
       };
-      nixosConfigurations."ins-iso" = lib.nixosSystem {
+      nixosConfigurations."installer" = lib.nixosSystem {
         inherit system;
         modules = [
           ({ pkgs, modulesPath, ... }: {
-            imports = [ (modulesPath + "/installer/cd-dvd/installation-cd-minimal.nix") ];
-            isoImage.squashfsCompression = "gzip -Xcompression-level 1";
-            environment.systemPackages = with pkgs; [
-              git
+            imports = [ ./hosts/installer ];
+            environment.systemPackages = [
               self.packages.${system}.nixos-fs-init
               self.packages.${system}.nixos-fs-mount
             ];
-            nix.settings = {
-              auto-allocate-uids = true;
-              auto-optimise-store = true;
-              experimental-features = "auto-allocate-uids cgroups nix-command flakes";
-              substituters = [
-                "https://mirrors.ustc.edu.cn/nix-channels/store"
-                "https://slaier.cachix.org"
-                "https://nix-community.cachix.org"
-              ];
-              trusted-public-keys = [
-                "slaier.cachix.org-1:NyXPOqlxuGWgyn0ApNHMopkbix3QjMUAcR+JOjjxLtU="
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-              ];
-              use-cgroups = true;
-            };
           })
         ];
       };
