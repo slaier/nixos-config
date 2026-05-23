59c65ee28318a7c1229369a56ec37677c46ca72d
slaier <slaier@users.noreply.github.com>
2026-05-23 12:20:56 +0800

    refactor: restructure Firefox bookmarks and update UI settings

--- modules/firefox/home.nix
+++ modules/firefox/home.nix
@@ -66,153 +66,89 @@ with lib;
         force = true;
         settings = [
           {
-            name = "Nix sites";
+            toolbar = true;
             bookmarks = [
               {
-                name = "NUR search";
-                url = "https://nur.nix-community.org/";
+                name = "Nix sites";
+                bookmarks = [
+                  {
+                    name = "NUR search";
+                    url = "https://nur.nix-community.org/";
+                  }
+                  {
+                    name = "Nix Manual";
+                    url = "https://nixos.org/manual/nix/stable/";
+                  }
+                  {
+                    name = "Nixpkgs Manual";
+                    url = "https://ryantm.github.io/nixpkgs/";
+                  }
+                  {
+                    name = "Noogle";
+                    url = "https://noogle.dev/";
+                  }
+                ];
               }
               {
-                name = "Nix Manual";
-                url = "https://nixos.org/manual/nix/stable/";
+                name = "Learn";
+                bookmarks = [
+                  {
+                    name = "Rust OS";
+                    url = "https://learningos.github.io/rust-based-os-comp2022/";
+                  }
+                  {
+                    name = "nLab";
+                    url = "https://ncatlab.org/nlab/show/HomePage";
+                  }
+                ];
               }
               {
-                name = "Nixpkgs Manual";
-                url = "https://ryantm.github.io/nixpkgs/";
+                name = "Collection";
+                bookmarks = [
+                  {
+                    name = "ACGN";
+                    url = "https://www.myiys.com/";
+                  }
+                  {
+                    name = "MirrorZ";
+                    url = "https://mirrorz.org/site";
+                  }
+                  {
+                    name = "Pling";
+                    url = "https://www.pling.com/";
+                  }
+                ];
               }
               {
-                name = "Noogle";
-                url = "https://noogle.dev/";
+                name = "Post";
+                bookmarks = [
+                  {
+                    name = "Proxy Env";
+                    url = "https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy";
+                  }
+                  {
+                    name = "Google Language Codes";
+                    url = "https://sites.google.com/site/tomihasa/google-language-codes";
+                  }
+                ];
+              }
+              {
+                name = "Dns Lookup";
+                url = "https://dnslookup.online/";
               }
             ];
-          }
-          {
-            name = "Learn";
-            bookmarks = [
-              {
-                name = "Rust OS";
-                url = "https://learningos.github.io/rust-based-os-comp2022/";
-              }
-              {
-                name = "nLab";
-                url = "https://ncatlab.org/nlab/show/HomePage";
-              }
-            ];
-          }
-          {
-            name = "Collection";
-            bookmarks = [
-              {
-                name = "ACGN";
-                url = "https://www.myiys.com/";
-              }
-              {
-                name = "MirrorZ";
-                url = "https://mirrorz.org/site";
-              }
-              {
-                name = "Pling";
-                url = "https://www.pling.com/";
-              }
-            ];
-          }
-          {
-            name = "Post";
-            bookmarks = [
-              {
-                name = "Proxy Env";
-                url = "https://about.gitlab.com/blog/2021/01/27/we-need-to-talk-no-proxy";
-              }
-              {
-                name = "Google Language Codes";
-                url = "https://sites.google.com/site/tomihasa/google-language-codes";
-              }
-            ];
-          }
-          {
-            name = "Dns Lookup";
-            url = "https://dnslookup.online/";
           }
         ];
       };
       settings = {
         "browser.urlbar.suggest.topsites" = false;
         "browser.warnOnQuitShortcut" = false;
-        "browser.toolbars.bookmarks.visibility" = "never";
-        "browser.uiCustomization.state" = ''
-          {
-              "placements": {
-                  "widget-overflow-fixed-list": [],
-                  "unified-extensions-area": [
-                      "i_diygod_me-browser-action",
-                      "_ublacklist-browser-action",
-                      "adnauseam_rednoise_org-browser-action",
-                      "jid1-kkzogwgsw3ao4q_jetpack-browser-action",
-                      "keepassxc-browser_keepassxc_org-browser-action",
-                      "offline-qr-code_rugk_github_io-browser-action",
-                      "tongwen_softcup-browser-action",
-                      "_74145f27-f039-47ce-a470-a662b129930a_-browser-action",
-                      "_b86e4813-687a-43e6-ab65-0bde4ab75758_-browser-action"
-                  ],
-                  "nav-bar": [
-                      "back-button",
-                      "forward-button",
-                      "stop-reload-button",
-                      "customizableui-special-spring1",
-                      "urlbar-container",
-                      "customizableui-special-spring2",
-                      "downloads-button",
-                      "bookmarks-menu-button",
-                      "_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action",
-                      "_e2488817-3d73-4013-850d-b66c5e42d505_-browser-action",
-                      "_446900e4-71c2-419f-a6a7-df9c091e268b_-browser-action",
-                      "offline-qr-code_rugk_github_io-browser-action",
-                      "fxa-toolbar-menu-button",
-                      "unified-extensions-button"
-                  ],
-                  "toolbar-menubar": [
-                      "menubar-items"
-                  ],
-                  "TabsToolbar": [
-                      "tabbrowser-tabs",
-                      "new-tab-button",
-                      "alltabs-button"
-                  ],
-                  "PersonalToolbar": [
-                      "personal-bookmarks"
-                  ]
-              },
-              "seen": [
-                  "i_diygod_me-browser-action",
-                  "_ublacklist-browser-action",
-                  "adnauseam_rednoise_org-browser-action",
-                  "jid1-kkzogwgsw3ao4q_jetpack-browser-action",
-                  "keepassxc-browser_keepassxc_org-browser-action",
-                  "offline-qr-code_rugk_github_io-browser-action",
-                  "tongwen_softcup-browser-action",
-                  "_74145f27-f039-47ce-a470-a662b129930a_-browser-action",
-                  "_aecec67f-0d10-4fa7-b7c7-609a2db280cf_-browser-action",
-                  "_b86e4813-687a-43e6-ab65-0bde4ab75758_-browser-action",
-                  "_e2488817-3d73-4013-850d-b66c5e42d505_-browser-action",
-                  "developer-button"
-              ],
-              "dirtyAreaCache": [
-                  "unified-extensions-area",
-                  "nav-bar"
-              ],
-              "currentVersion": 20,
-              "newElementCount": 2
-          }
-        '';
+        "browser.toolbars.bookmarks.visibility" = "newtab";
         "extensions.activeThemeID" = "elemental-soft-colorway@mozilla.org";
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-        "layout.css.color-mix.enabled" = true;
-        "layout.css.has-selector.enabled" = true;
         "svg.context-properties.content.enabled" = true;
-        "userChrome.Tabs.Option7.Enabled" = true;
-        "userChrome.TabSeparators.Saturation.Medium.Enabled" = true;
-        "userChrome.Menu.Size.Compact.Enabled" = true;
-        "userChrome.Menu.Icons.Regular.Enabled" = true;
+        "browser.newtabpage.activity-stream.nova.enabled" = false;
+        "widget.gtk.rounded-bottom-corners.enabled" = true;
       };
       search = {
         default = "Google NCR";
