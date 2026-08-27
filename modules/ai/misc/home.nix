11599bc14b178bf42477ecabab7812f6a8c07dff
slaier <slaier@users.noreply.github.com>
2026-08-27 22:59:28 +0800

    feat(ai): move qwentts package from sillytavern to home packages

--- modules/ai/misc/home.nix
+++ modules/ai/misc/home.nix
@@ -7,6 +7,7 @@
 {
   home.packages = with pkgs; [
     geminicommit
+    qwentts
   ];
 
   sops.secrets = {
--- modules/ai/sillytavern/default.nix
+++ modules/ai/sillytavern/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 let
@@ -21,5 +20,4 @@ in
       };
     };
   };
-  environment.systemPackages = [ pkgs.qwentts ];
 }
